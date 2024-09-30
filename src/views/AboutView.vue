712177f67d0bcd0d40bd0363695977ef180edc94
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2024-09-30 09:14:24 +0300

    things i learnt on gsap 29th-semptember-2024

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,7 @@
   <div class="div1">hey</div>
   <div class="div2">
       <div class="square"></div>
+      <div class="square2"></div>
   </div>
 </template>
 
@@ -14,9 +15,35 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 // Register the ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
 
+// Use onMounted lifecycle hook to ensure the DOM is ready
+onMounted(() => {
+  // Apply GSAP animations after the component is mounted
+  gsap.to(".square", {
+    // x: 700,       // Move the square horizontally by 700px
+    duration: 3,  // Animation duration of 3 seconds
+    scrollTrigger: {
+      trigger:".square2",
+      start:"top 80%",
+      end:"top 30%",
+      scrub: 4,
+      pin:".square",
+      pinSpacing: true,
+      toggleActions:"play none none none",
+      // toggleClass:"red",
+      markers:{
+        startColor:"purple",
+        endColor:"fuchisa",
+        fontSize:"3rem",
+      },
 
-
+    },
+    // ease: "power1.out",
+    // yoyo:true,
+    // repeat:-1,
+  });
+});
 </script>
+
 
 <style scoped>
 .div1, .div2{
@@ -32,5 +59,14 @@ gsap.registerPlugin(ScrollTrigger);
   width: 150px;
   height: 150px;
   background: fuchsia;
+}
+.square2{
+  margin-top: 300px;
+  width: 150px;
+  height: 150px;
+  background: blue;
+}
+.red{
+  background-color: brown;
 }
 </style>
